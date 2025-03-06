5c3110ea581d3f7f69b9ebff6f34d124e66052ae
Odima-dev <vincentodima22@gmail.com>
2025-03-06 11:16:34 +0300

    creating year and temperature columns

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ async function getData() {
     const table = data.split('\n').slice(1)
 
     table.forEach(row => {
-        columns = row.split(',')
+        const columns = row.split(',')
+        const year = columns[0]
+        const temperature = columns[1]
+        console.log(columns)
     })
 }
 
