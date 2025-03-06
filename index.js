7e27f02864ce994156a4d534a3caf93c0ea14787
Odima-dev <vincentodima22@gmail.com>
2025-03-06 11:14:03 +0300

    creating table columns and rows

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ async function getData() {
     response = await fetch("ZonAnn.Ts+dSST.csv")
     const data = await response.text()
     const table = data.split('\n').slice(1)
+
+    table.forEach(row => {
+        columns = row.split(',')
+    })
 }
 
 getData()
